87049bc737079d5f8d8241dcc1965a78b932cad9
zerihun-cs <zerihun.cs@gmail.com>
2022-05-11 18:15:17 +0300

    sequences in python

---
+++ sequences.py
@@ -0,0 +1,35 @@
+# A sequence is a positionally ordered collection of items. 
+# And you can refer to any item in the sequence by using its index number e.g., s[0] and s[1].
+# In Python, the sequence index starts at 0, not 1. So the first element is s[0] and the second element is s[1].
+# If the sequence s has n items, the last item is s[n-1].
+
+
+
+# Python lists are similar to an array but they allow us to create a heterogeneous collection of items inside a list.
+# A list can contain numbers, strings, lists, tuples, dictionaries, objects, etc.
+
+# Lists are declared by using square brackets around comma-separated items.
+
+# Lists are mutable which makes it easier to change and we can quickly modify a list by directly accessing it.
+name_list = ['hanna','abel','john','anania',23,4]
+
+
+# A Set is an unordered collection data type that is iterable, mutable and has no duplicate elements. 
+# Python’s set class represents the mathematical notion of a set.
+
+#Since sets are unordered, we cannot access items using indexes like we do in lists.
+name_set = {'hanna','abel','john','anania','abel'}
+
+
+# Python Tuples
+
+# Tuples are also a sequence of Python objects. 
+# A tuple is created by separating items with a comma.
+# They can be optionally put inside the parenthesis () but it is necessary to put parenthesis in an empty tuple.
+name_tuple = ('hanna','abel','john','anania','abel')
+
+# Python dictionary is an unordered collection of items. 
+# Each item of a dictionary has a key/value pair.
+# Dictionaries cannot have two items with the same key:
+name_dict ={'name':'zerihun', 'age':28,'geneder':'male'}
+
