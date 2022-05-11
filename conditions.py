951c55034a793ac62bfb9f84d28d13fa32cd9b6d
zerihun-cs <zerihun.cs@gmail.com>
2022-05-11 18:26:13 +0300

    conditional statements in python

---
+++ conditions.py
@@ -0,0 +1,22 @@
+# Python if Statement Syntax:
+
+# if expression
+#  Statement
+
+# elif expression
+#  Statement
+
+# else 
+#  Statement
+
+age,another_age =18,24
+
+if(age < another_age):
+	st= "age is less than another_age"
+ 
+elif (age == another_age):
+		st= "x is same as y"
+  
+else:
+	st= "age is greater than another_age"
+print (st)
